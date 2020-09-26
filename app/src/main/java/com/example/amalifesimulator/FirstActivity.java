8cc8eab9088b96536ce51da0769f4addc8d2dd83
Astatium5 <dmitro00@bk.ru>
2020-09-26 11:39:24 -0400

    all the activities created are combined in one, which uses a viewFlipper

--- app/src/main/java/com/example/amalifesimulator/FirstActivity.java
+++ app/src/main/java/com/example/amalifesimulator/FirstActivity.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class FirstActivity extends AppCompatActivity {
+    private Integer choice;
+    private String firstName;
+    private String lastName;
+    private ViewFlipper viewFlipper;
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_first);
 
-        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        viewFlipper = findViewById(R.id.viewFlipperStart);
+
+//        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
 
 //        requestWindowFeature(Window.FEATURE_NO_TITLE);
 //        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -21,9 +34,66 @@ public class FirstActivity extends AppCompatActivity {
     }
 
 
-    public void onNextButton (View view) {
-        Intent secondActivityIntent = new Intent(this, SecondActivity.class);
+    public void onNextButtonFirst (View view) {
+        viewFlipper.showNext();
+    }
 
-        startActivity(secondActivityIntent);
+    public void onRadioButtonClicked (View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.firstPersonRadioButton:
+                if (checked)
+                    choice = new Integer(0);
+                break;
+            case R.id.secondPersonRadioButton:
+                if (checked)
+                    choice = new Integer(1);
+                break;
+            case R.id.thirdPersonRadioButton:
+                if (checked)
+                    choice = new Integer(2);
+                break;
+            case R.id.fourthPersonRadioButton:
+                if (checked)
+                    choice = new Integer(3);
+                break;
+        }
+    }
+
+    public void onNextButtonSecond (View view) {
+        EditText firstNameEditText = findViewById(R.id.firstNameEditText);
+        EditText lastNameEditText = findViewById(R.id.lastNameEditText);
+
+        firstNameEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if ()
+            }
+        });
+
+        lastNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                lastName = lastNameEditText.getText().toString();
+            }
+        });
+
+        viewFlipper.showNext();
+    }
+
+    public void onNextButtonThird (View view) {
+        viewFlipper.showNext();
+
+        TextView helloTextView = findViewById(R.id.helloTextView);
+
+        String temp = "Hello " + firstName + " " + lastName + "!";
+        helloTextView.setText(temp);
     }
 }
--- app/src/main/java/com/example/amalifesimulator/Person.java
+++ app/src/main/java/com/example/amalifesimulator/Person.java
@@ -3,69 +3,49 @@ package com.example.amalifesimulator;
 public class Person {
     private String firstName;
     private String lastName;
-    private Integer age;
+//    private Integer age;
     private Integer health;
     private Integer money;
-    private Integer sex; // 0 for male, 1 for female
+    private Integer credits;
+    private Integer socialLifePoints;
+    private Integer careerPoints;
 
-    public Person(String firstName, String lastName, Integer age, Integer health, Integer money, Integer sex) {
+    public Person (String firstName, String lastName, Integer health, Integer money,
+                    Integer credits, Integer socialLifePoints, Integer careerPoints) {
         setFirstName(firstName);
         setLastName(lastName);
-        setAge(age);
         setHealth(health);
         setMoney(money);
-        setSex(sex);
+        setCredits(credits);
+        setSocialLifePoints(socialLifePoints);
+        setCareerPoints(careerPoints);
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getFirstName () { return firstName; }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public void setFirstName (String firstName) { this.firstName = firstName; }
 
-    public String getLastName() {
-        return lastName;
-    }
+    public String getLastName () { return lastName; }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public void setLastName (String lastName) { this.lastName = lastName; }
 
-    public Integer getAge() {
-        return age;
-    }
+    public Integer getHealth () { return health; }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+    public void setHealth (Integer health) { this.health = health; }
 
-    public Integer getHealth() {
-        return health;
-    }
+    public Integer getMoney () { return money; }
 
-    public void setHealth(Integer health) {
-        this.health = health;
-    }
+    public void setMoney (Integer money) { this.money = money; }
 
-    public Integer getMoney() {
-        return money;
-    }
+    public Integer getCredits () { return credits; }
 
-    public void setMoney(Integer money) {
-        this.money = money;
-    }
+    public void setCredits (Integer credits) { this.credits = credits; }
 
-    public Integer getSex() {
-        return sex;
-    }
+    public Integer getSocialLifePoints () { return socialLifePoints; }
 
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
+    public void setSocialLifePoints (Integer socialLifePoints) { this.socialLifePoints = socialLifePoints; }
 
-    public void incrementAge() {
-        age++;
-    }
+    public Integer getCareerPoints () { return careerPoints; }
+
+    public void setCareerPoints (Integer careerPoints) { this.careerPoints = careerPoints; }
 }
